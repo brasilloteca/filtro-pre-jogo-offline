2f75ce9741a121b6a48e4cf79d6d4e3aa021971b
brasilloteca <brasilloteca@gmail.com>
2025-11-09 17:23:38 -0300

    Enhance service worker with caching strategies
    Add cache management during activation and fetch events.

--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,28 @@ self.addEventListener("install", event => {
   );
 });
 
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
+    )
+  );
+});
+
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+      return (
+        response ||
+        fetch(event.request)
+          .then(res => {
+            return caches.open(CACHE_NAME).then(cache => {
+              cache.put(event.request, res.clone());
+              return res;
+            });
+          })
+          .catch(() => caches.match("./index.html"))
+      );
     })
   );
 });
